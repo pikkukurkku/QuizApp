023c7e1634599b61e050cb596d84a45e2bf5e025
pikkukurkku <natalia.ogorek@gmail.com>
2025-03-28 10:12:12 +0100

    Initial Commit

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Natalia Ogorek on 28.03.25.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuizAppTests/QuizAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppTests.swift
+//  QuizAppTests
+//
+//  Created by Natalia Ogorek on 28.03.25.
+//
+
+import Testing
+@testable import QuizApp
+
+struct QuizAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
